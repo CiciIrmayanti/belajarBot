46d9ea7f3f28b147277ab6608a0d29ec214ef96c
CiciIrmayanti <55769697+CiciIrmayanti@users.noreply.github.com>
2020-06-12 14:47:23 +0700

    Add files via upload

---
+++ mytoken.py
@@ -0,0 +1,4 @@
+TOKEN='1240482534:AAEWz3bCjudF1Lt21-Ohz1QTo09EfuNftqE'
+SAPA=" \U0001F970 Selamat Datang Di StarbhakBot \n \n \U0001F916 Saya StarbhakBot \U0001F916 \n \nDisini kamu bisa melihat " \
+     "data siswa dengan perintah : \n" \
+     " /datasiswa \n\n\n"
